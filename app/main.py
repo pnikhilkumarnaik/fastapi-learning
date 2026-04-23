61a2ca580a95f4d1e4cdf716d00641e1baaa0e51
P Nikhil Kumar Naik <pnikhilkumarnaik@gmail.com>
2026-04-23 16:45:12 +0530

    add root endpoint

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from app.database import engine
 from app import models
 from app.routers import blog, user,authentication
 app=FastAPI()
+@app.get("/")
+def root():
+    return {"message": "FastAPI running on Render 🚀"}
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(authentication.router)
@@ -10,6 +13,3 @@ app.include_router(blog.router)
 app.include_router(user.router)
 
 
-@app.get("/")
-def root():
-    return {"message": "FastAPI running 🚀"}
